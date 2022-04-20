e72f34c736d41eec1e7103a023223a43220ccec8
xlCyanz <johanse.linares@gmail.com>
2022-04-19 22:07:01 -0400

    Updated...

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -4,7 +4,7 @@ window.jsPDF = window.jspdf.jsPDF;
 
 document.getElementById("logOutButton").addEventListener("click", () => {
     localStorage.removeItem("token");
-    window.location.href = "/";
+    window.location.href = "/index.html";
 });
 
 const createElement = (element, children, attributes) => {
